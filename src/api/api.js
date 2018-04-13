0c00ebd293b5389176b8cf45784d53396ca5370b
bootstet <bootstet@163.com>
2018-04-13 16:38:52 +0800

    端口修改

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 /* 配置基础的一些请求路径 */
 import axios from 'axios';
 // 配置为日常的请求的域名,后台数据的请求路径配置
-let base = 'http://test-manage.dianliaoapp.com/ydlManage/server/index.php';
-// let base = 'https://manage.dianliaoapp.com/ydlManage/server/index.php';
+// let base = 'http://test-manage.dianliaoapp.com/ydlManage/server/index.php';
+let base = 'https://manage.dianliaoapp.com/ydlManage/server/index.php';
 // 检测判断是否在正式服上面，修改相应对应的正式服路径
 if (location.href.indexOf('https://manage.dianliaoapp.com')=='0') {
     base = 'https://manage.dianliaoapp.com/ydlManage/server/index.php';
